c4c989979ce484ea919088bcf8c5ecbe40fd8253
joeyhendricks <joeyhendricks20@gmail.com>
2020-09-28 21:10:23 +0200

    added url

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=['QuickPotato', 'QuickPotato.harness', 'QuickPotato.database', 'QuickPotato.inspect',
               'QuickPotato.utilities', 'QuickPotato.utilities.tests', 'QuickPotato.utilities.exceptions',
               'QuickPotato.statistics', 'QuickPotato.configuration'],
-    url='',
+    url='https://github.com/JoeyHendricks/python-unit-level-performance-testing',
     license='MIT',
     author='Joey Hendricks',
     author_email='joey.hendricks20@icloud.com',
