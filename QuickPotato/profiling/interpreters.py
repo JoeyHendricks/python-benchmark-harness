fbb7915515ffbf3bd2f3a56d295ec65d20632317
joeyhendricks <joeyhendricks20@gmail.com>
2020-12-25 00:54:14 +0100

    Fixed the following issue: #1

--- QuickPotato/profiling/interpreters.py
+++ QuickPotato/profiling/interpreters.py
@@ -12,6 +12,7 @@ class PerformanceStatisticsInterpreter(Crud):
 
         self.performance_statistics = performance_statistics
         self.total_response_time = total_response_time
+        self.using_server_less_database = bool(self._validate_connection_url(database_name)[0:6] == "sqlite")
 
         self.database_name = database_name
         self.method_name = method_name
@@ -35,7 +36,7 @@ class PerformanceStatisticsInterpreter(Crud):
         """
         asyncio.set_event_loop(asyncio.new_event_loop())
         loop = asyncio.get_event_loop()
-        loop.run_in_executor(executor=None, func=self.send_payload)
+        loop.run_in_executor(executor=None, func=self.send_payload_to_database)
 
     def upload_payload_to_database_sync(self):
         """
@@ -43,21 +44,31 @@ class PerformanceStatisticsInterpreter(Crud):
         -------
 
         """
-        self.send_payload()
+        self.send_payload_to_database()
 
-    def send_payload(self):
+    def send_payload_to_database(self):
         """
-        :return:
-        """
-        payload = self.construct_writable_payload()
-        self.insert_performance_statistics(payload=payload, database=self.database_name)
-
-    def construct_writable_payload(self):
-        """
-
         :return:
         """
         payload = []
+        for row in self.iterate_through_profiled_stack():
+
+            # Dividing payload into multiple inserts to work around server-less variable restrictions
+            if self.using_server_less_database is True and len(payload) == 999:
+                # Sending and nuking payload variable when exceeding SQLite's max amount of variables.
+                self.insert_performance_statistics(payload=payload, database=self.database_name)
+                payload = []
+
+            payload.append(row)
+
+        # Inserting full payload into server-based database or sending left-overs to sever-less database
+        self.insert_performance_statistics(payload=payload, database=self.database_name)
+
+    def iterate_through_profiled_stack(self):
+        """
+
+        :return:
+        """
         try:
             for function, (cc, nc, tt, ct, callers) in self.performance_statistics.items():
 
@@ -69,9 +80,8 @@ class PerformanceStatisticsInterpreter(Crud):
                 child_line_number = function[1]
                 child_function_name = function[2]
 
-                for i in callers:
-                    payload.append(
-                        {
+                for row in callers:
+                        yield {
                             "test_id": self.test_id,
                             "sample_id": self.sample_id,
                             "test_case_name": self.database_name,
@@ -81,17 +91,14 @@ class PerformanceStatisticsInterpreter(Crud):
                             "child_path": child_path,
                             "child_line_number": child_line_number,
                             "child_function_name": child_function_name,
-                            "parent_path": i[0],
-                            "parent_line_number": i[1],
-                            "parent_function_name": i[2],
+                            "parent_path": row[0],
+                            "parent_line_number": row[1],
+                            "parent_function_name": row[2],
                             "number_of_calls": nc,
                             "total_time": tt,
                             "cumulative_time": ct,
                             "total_response_time": self.total_response_time
                         }
-                    )
-
-            return payload
 
         except Exception:
             raise AgentCannotProcessProfilerOutput()
