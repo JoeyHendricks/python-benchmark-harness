8bae7ee8383a86daf6d93505d3e92fb9e873377c
joeyhendricks20@gmail.com <joeyhendricks20@gmail.com>
2021-01-20 23:36:25 +0100

    created wheel for version 0.2.

--- setup.py
+++ setup.py
@@ -2,13 +2,32 @@ from setuptools import setup
 
 setup(
     name='QuickPotato',
-    version='0.1',
-    packages=['QuickPotato', 'QuickPotato.harness', 'QuickPotato.profiling', 'QuickPotato.database',
-              'QuickPotato.utilities', 'QuickPotato.statistical', 'QuickPotato.configuration'],
-    url='https://github.com/JoeyHendricks/python-unit-level-performance-testing',
+    version='0.2',
+    packages=[
+        'QuickPotato',
+        'QuickPotato.harness',
+        'QuickPotato.database',
+        'QuickPotato.profiling',
+        'QuickPotato.utilities',
+        'QuickPotato.statistical',
+        'QuickPotato.configuration',
+        'QuickPotato.visualization'
+    ],
+    install_requires=[
+        'numpy',
+        'SQLAlchemy-Utils',
+        'scipy',
+        'SQLAlchemy',
+        'PyMySQL',
+        'psutil',
+        'pandas',
+        'PyYAML',
+        'Jinja2'
+    ],
+    url='https://github.com/JoeyHendricks/QuickPotato',
     license='MIT',
     author='Joey Hendricks',
     author_email='joey.hendricks20@icloud.com',
-    description='The framework that enables Python developers to include performance validation into their unit tests',
-    install_requires=['numpy', 'SQLAlchemy-Utils', 'scipy', 'SQLAlchemy', 'PyMySQL', 'psutil', 'pandas', 'PyYAML']
+    description='The framework that enables Python developers to include performance validation into their tests',
+    python_requires='>=3.7'
 )
