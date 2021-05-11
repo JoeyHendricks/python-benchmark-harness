c0cc5dcb2503ed090189a19640f2d8149ce7d492
joeyhendricks20@gmail.com <joeyhendricks20@gmail.com>
2021-05-11 18:15:47 +0200

    updated the visualization example code blocks in the read me

--- examples/non-intrusive performance testing/non_intrusive_quick_profiling.py
+++ examples/non-intrusive performance testing/non_intrusive_quick_profiling.py
@@ -1,18 +1,17 @@
 from examples.example_code import FancyCode
 from QuickPotato.profiling.intrusive import performance_test as pt
-from QuickPotato.statistical.visualizations import FlameGraph, HeatMap
+from QuickPotato.statistical.visualizations import CsvFile
 
 # Create a test case
-pt.test_case_name = "exploratory performance test"
+pt.test_case_name = "exporting to csv"
 
 # Attach the method from which you want to performance test
 pt.measure_method_performance(
     method=FancyCode().say_my_name_and_more,  # <-- The Method which you want to test.
-    arguments=["joey hendricks"],  # <-- You're arguments go here.
+    arguments=["joey hendricks"],  # <-- Your arguments go here.
     iteration=10,  # <-- The number of times you want to execute this method.
     pacing=0  # <-- How much seconds you want to wait between iterations.
 )
 
-# Generate visualizations to analyse your code.
-FlameGraph(test_case_name=pt.test_case_name, test_id=pt.current_test_id).export("C:\\temp\\")
-HeatMap(test_case_name=pt.test_case_name, test_ids=[pt.current_test_id, pt.previous_test_id]).export("C:\\temp\\")
+# Export the sample into csv file for further analysis
+CsvFile(test_case_name=pt.test_case_name, test_id=pt.current_test_id).export("C:\\temp\\")
