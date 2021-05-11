d382d00efe83123dd94df66623a690bd6d2ba1ed
joeyhendricks20@gmail.com <joeyhendricks20@gmail.com>
2021-05-11 18:16:24 +0200

    updated the examples

--- examples/non-intrusive performance testing/non_intrusive_quick_profiling.py
+++ examples/non-intrusive performance testing/non_intrusive_quick_profiling.py
@@ -1,9 +1,9 @@
 from examples.example_code import FancyCode
 from QuickPotato.profiling.intrusive import performance_test as pt
-from QuickPotato.statistical.visualizations import CsvFile
+from QuickPotato.statistical.visualizations import FlameGraph, HeatMap, CsvFile
 
 # Create a test case
-pt.test_case_name = "exporting to csv"
+pt.test_case_name = "exploratory performance test"
 
 # Attach the method from which you want to performance test
 pt.measure_method_performance(
@@ -13,5 +13,7 @@ pt.measure_method_performance(
     pacing=0  # <-- How much seconds you want to wait between iterations.
 )
 
-# Export the sample into csv file for further analysis
+# Generate visualizations to analyse your code.
+FlameGraph(test_case_name=pt.test_case_name, test_id=pt.current_test_id).export("C:\\temp\\")
+HeatMap(test_case_name=pt.test_case_name, test_ids=[pt.current_test_id, pt.previous_test_id]).export("C:\\temp\\")
 CsvFile(test_case_name=pt.test_case_name, test_id=pt.current_test_id).export("C:\\temp\\")
