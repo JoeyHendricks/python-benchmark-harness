8bb0dd262bf451dfd2e9c7dc7351e687ea050917
joeyhendricks20@gmail.com <joeyhendricks20@gmail.com>
2022-01-01 22:52:39 +0100

    deleted a default config which was no longer needed

--- QuickPotato/utilities/defaults.py
+++ QuickPotato/utilities/defaults.py
@@ -1,29 +1,4 @@
 
-template_for_service_level_agreement_threshold_policy = {
-    "max_and_min_boundary_for_average": {"max": None, "min": None},
-    "max_and_min_boundary_for_max_allowed_outlier": {"max": None, "min": None},
-    "max_and_min_boundary_for_min_allowed_outlier": {"max": None, "min": None},
-    "max_and_min_boundary_for_percentile_5th": {"max": None, "min": None},
-    "max_and_min_boundary_for_percentile_10th": {"max": None, "min": None},
-    "max_and_min_boundary_for_percentile_15th": {"max": None, "min": None},
-    "max_and_min_boundary_for_percentile_20th": {"max": None, "min": None},
-    "max_and_min_boundary_for_percentile_25th": {"max": None, "min": None},
-    "max_and_min_boundary_for_percentile_30th": {"max": None, "min": None},
-    "max_and_min_boundary_for_percentile_35th": {"max": None, "min": None},
-    "max_and_min_boundary_for_percentile_40th": {"max": None, "min": None},
-    "max_and_min_boundary_for_percentile_45th": {"max": None, "min": None},
-    "max_and_min_boundary_for_percentile_50th": {"max": None, "min": None},
-    "max_and_min_boundary_for_percentile_55th": {"max": None, "min": None},
-    "max_and_min_boundary_for_percentile_60th": {"max": None, "min": None},
-    "max_and_min_boundary_for_percentile_65th": {"max": None, "min": None},
-    "max_and_min_boundary_for_percentile_70th": {"max": None, "min": None},
-    "max_and_min_boundary_for_percentile_75th": {"max": None, "min": None},
-    "max_and_min_boundary_for_percentile_80th": {"max": None, "min": None},
-    "max_and_min_boundary_for_percentile_85th": {"max": None, "min": None},
-    "max_and_min_boundary_for_percentile_90th": {"max": None, "min": None},
-    "max_and_min_boundary_for_percentile_95th": {"max": None, "min": None},
-}
-
 default_quick_potato_configuration = {
 
     "enable_intrusive_profiling": True,
