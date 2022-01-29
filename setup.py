15c8781fa8b6d4837108b8d1784f266284e8c6ba
joeyhendricks20@gmail.com <joeyhendricks20@gmail.com>
2022-01-29 14:08:48 +0100

    fixed setup.py

--- setup.py
+++ setup.py
@@ -1,32 +1,34 @@
 from setuptools import setup
-from Benchmarking import __version__
 
 setup(
-    name='Benchmarking',
-    version=str(__version__),
+    name='python-micro-benchmarks',
+    version='1.0.2',
     packages=[
         'Benchmarking',
-        'Benchmarking.profiling',
         'Benchmarking._database',
         'Benchmarking.profiling',
+        'Benchmarking._templates',
         'Benchmarking._utilities',
         'Benchmarking.statistical',
-        'Benchmarking._configuration'
+        'Benchmarking._configuration',
+        'Benchmarking.visualizations'
     ],
     install_requires=[
-        'numpy',
-        'SQLAlchemy-Utils',
-        'scipy',
         'SQLAlchemy',
+        'SQLAlchemy-Utils',
+        'setuptools',
+        'numpy',
         'pandas',
+        'Jinja2',
+        'scipy',
         'PyYAML',
-        'Jinja2'
+        'plotly'
     ],
+    package_dir={'': 'package'},
     url='https://github.com/JoeyHendricks/python-benchmarks',
     license='MIT',
     author='Joey Hendricks',
     author_email='joey.hendricks20@icloud.com',
-    description='A micro/macro benchmark framework for the Python programming '
-                'language that helps with optimizing your software.',
-    python_requires='>=3.8'
+    description='A micro/macro benchmark framework for the Python '
+                'programming language that helps with optimizing your software.'
 )
