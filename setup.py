b2cfa630ab42fcbdca5552d444af3226180c0e94
joeyhendricks20@gmail.com <joeyhendricks20@gmail.com>
2022-02-03 21:24:09 +0100

    updated version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='python-benchmark-harness',
-    version='1.0.2',
+    version='1.0.3',
     packages=[
         'Benchmarking',
         'Benchmarking._database',
